c78f5a4e6b52cc9cc6c2c35b04b3c9557a53efa8
Samin Shrestha <saminshrestha7@gmail.com>
2026-01-16 19:02:42 +0545

    added draft-list

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,7 +3,7 @@ from blog_app.models import Post
 # Create your views here.
 
 def post_list(request):
-    posts= Post.objects.all()
+    posts= Post.objects.filter(published_at__isnull=False)
     return render(
         request, 
         'post_list.html', 
@@ -16,4 +16,12 @@ def post_detail(request, pk):
         request,
         'post_detail.html',
         {'post': post},
+    )
+    
+def draft_list(request):
+    posts= Post.objects.filter(published_at__isnull=True)
+    return render(
+        request, 
+        'draft_list.html', 
+        {'posts': posts},
     )
